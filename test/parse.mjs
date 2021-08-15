38bd933ff8729d102e58ae1ae06d5178d6a41daa
Huang-Huang Bao <eh5@sokka.cn>
2021-08-15 19:08:50 +0800

    feature(parse): add a working parser
    Use tokenizer(lexer) cloned from spa/utils/json.h to maxiumize compatibility with SPA's implementation.
    Switch from state machine to recursive parsing.
    
    Add several test cases using sole assert module, we might want to switch to other more advanceded test framework

--- test/parse.mjs
+++ test/parse.mjs
@@ -1,25 +1,67 @@
+import assert from 'assert'
+import { readFileSync } from 'fs'
 import { parse } from '../lib/index.mjs'
 
-console.time('parse')
-const obj = parse(`
+assert.equal(
+  parse('# line comment \na'),
+  'a'
+)
 
-#
-{
-    apple = 1
-    b = string1
-    c = [
-        2,
-        string2,
-        {a : 1}
-    ]
-    d = {
-        e = 3
-    }
-    f: "string3"
-}
-`)
-console.timeEnd('parse')
+assert.equal(
+  parse('false'),
+  false
+)
 
-const str = JSON.stringify(obj, null, 2)
+assert.equal(
+  parse('null'),
+  null
+)
 
-console.log(str)
+assert.equal(
+  parse('1'),
+  1
+)
+
+// bare string
+assert.equal(
+  parse(' 1\\\\abc$ '),
+  '1\\\\abc$'
+)
+
+// escape
+assert.equal(
+  parse('"\\\\"'),
+  '\\'
+)
+
+// top-level array
+assert.deepEqual(
+  parse('[{ a, 1 b=2 c:3, d 4, 1 2 }, str]'),
+  [{ a: 1, b: 2, c: 3, d: 4, "1": 2 }, 'str']
+)
+
+// nested
+assert.deepEqual(
+  parse('{ a { b = [ c: { d 1 } f ] } }'),
+  { a: { b: ['c', { d: 1 }, 'f'] } }
+)
+
+// native JSON
+const obj = { a: { b: ['c', { d: 1 }, 'f'] } }
+assert.deepEqual(
+  parse(JSON.stringify(obj)),
+  obj
+)
+
+const conf = readFileSync('./test/pipewire.conf')
+
+console.time('parse conf')
+
+const confObj = parse(conf)
+
+console.timeEnd('parse conf')
+
+assert.deepEqual(
+  confObj,
+  JSON.parse(readFileSync('./test/pipewire.conf.json')),
+)
